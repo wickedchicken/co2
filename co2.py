f0494ba5eef22dca0b3f1962d895c53ac04b30e0
Mike Stipicevic <stipredirect@gmail.com>
2023-07-23 22:33:13 +0200

    Remove adafruit and pushover

--- co2.py
+++ co2.py
@@ -2,18 +2,6 @@
 
 import sys, fcntl, time, os
 
-from Adafruit_IO import Client
-from pushover import Client as PushoverClient
-
-AIO_USER = os.getenv('AIO_USER')
-AIO_KEY = os.getenv('AIO_USER')
-
-PUSHOVER_CLIENT = os.getenv('PUSHOVER_CLIENT')
-PUSHOVER_TOKEN = os.getenv('PUSHOVER_TOKEN')
-
-aio = Client(AIO_USER, AIO_KEY)
-pushover = PushoverClient(PUSHOVER_CLIENT, api_token=PUSHOVER_TOKEN)
-
 def decrypt(key,  data):
     cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
     shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
@@ -53,8 +41,6 @@ if __name__ == "__main__":
     set_report = "\x00" + "".join(chr(e) for e in key)
     fcntl.ioctl(fp, HIDIOCSFEATURE_9, set_report)
 
-    too_high = False
-    already_sent = False
     while True:
         values = {}
 
@@ -80,17 +66,7 @@ if __name__ == "__main__":
                     print "RH: %2.2f" % (values[0x44]/100.0),
                 print
 
-        aio.send('co2-ppm', values[0x50])
-        aio.send('temperature-in-c', values[0x42]/16.0-273.15)
-
-        if values[0x50] > 1000 and not too_high:
-            too_high = True
-            if not already_sent:
-                pushover.send_message("Stoss your luft! co2 ppm is {}".format(values[0x50]))
-                already_sent = True
-        elif values[0x50] < 600 and too_high:
-            too_high = False
-            already_sent = False
-            pushover.send_message("Luft has been stossed! co2 ppm is {}".format(values[0x50]))
+        print 'co2 ppm {}'.format(values[0x50])
+        print 'temperature in c {}'.format(values[0x42]/16.0-273.15)
 
         time.sleep(60)
